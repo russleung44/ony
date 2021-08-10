6a639371d67abbe67cf44ef5b6fddf0449e50302
Tony <russ44@qq.com>
2021-08-10 17:06:40 +0800

    🎉 初始化 ony

---
+++ src/main/java/com/ony/OnyApplication.java
@@ -0,0 +1,16 @@
+package com.ony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author TonyLeung
+ */
+@SpringBootApplication
+public class OnyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnyApplication.class, args);
+    }
+
+}
